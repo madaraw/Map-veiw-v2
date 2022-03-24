73f4277393d0e8f87b97a8b193ffa40b12d1100b
madaraw <rachediwalid@yahoo.fr>
2022-03-24 11:09:13 +0100

    embeded the map and put a permanent drawer

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,29 @@
 <template>
-  <v-main></v-main>
+  <v-main> 
+    <v-navigation-drawer app permanent>
+    </v-navigation-drawer>
+    <div class="map pa-0 ma-0" ref="map"></div>
+    </v-main>
 </template>
 
 <script>
+export default {
+  mounted() {
+    let map = null;
+    let center = { lat: 53.3498, lng: -6.2603 };
+
+    // Embedding the map
+    map = new this.$google.maps.Map(this.$refs.map, {
+      zoom: 6,
+      center: center,
+      disableDefaultUI: true,
+    });
+  },
+};
 </script>
+
+<style>
+.map{
+  height: 100vh;
+}
+</style>
