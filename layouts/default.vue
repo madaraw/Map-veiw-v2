ba243dfeb88df443dc76160867f9aee2a2fe7b6e
madaraw <rachediwalid@yahoo.fr>
2022-03-28 10:25:27 +0200

    Styled the scroll bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,16 @@
     <Nuxt />
   </v-app>
 </template>
+
+<style>
+::-webkit-scrollbar {
+  width: 5px;
+  height: 8px;
+}
+::-webkit-scrollbar-track {
+    background-color: transparent;
+}
+::-webkit-scrollbar-thumb {
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+}
+</style>
