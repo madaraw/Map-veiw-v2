a334ba01f2cde4107d0447b73ae8064a87ae02ed
madaraw <rachediwalid@yahoo.fr>
2022-04-01 08:18:58 +0100

    Clustered the markers with a custom renderer

--- components/GMap.vue
+++ components/GMap.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+
 export default {
   name: "GMap",
   props: ["onlineCameras", "offlineCameras"],
@@ -33,7 +35,7 @@ export default {
             size: { width: 57, height: 31.5 },
             scaledSize: { width: 57, height: 31.5 },
           });
-          marker.setZIndex(marker.getZIndex() + ((cameras.length * 2) + 1));
+          marker.setZIndex(marker.getZIndex() + (cameras.length * 2 + 1));
         });
         marker.addListener("mouseout", () => {
           marker.setIcon({
@@ -42,12 +44,8 @@ export default {
               width: 38,
               height: 21,
             },
-            scaledSize: {
-              width: 38,
-              height: 21,
-            },
           });
-          marker.setZIndex(marker.getZIndex() - ((cameras.length * 2) + 1));
+          marker.setZIndex(marker.getZIndex() - (cameras.length * 2 + 1));
         });
         markers.push(marker);
       }
@@ -102,14 +100,13 @@ export default {
       });
       marker.setZIndex(
         marker.getZIndex() -
-          (this.onlineCameras.length +
-          this.offlineCameras.length +
-          1)
+          (this.onlineCameras.length + this.offlineCameras.length + 1)
       );
     },
   },
   mounted() {
     let center = { lat: 53.3498, lng: -6.2603 };
+    let clusterer = null;
 
     // Embedding the map
     this.map = new this.$google.maps.Map(this.$refs.map, {
@@ -144,7 +141,100 @@ export default {
       thumbIcon
     );
 
-    // console.log(marker);
+    clusterer = new MarkerClusterer({
+      map: this.map,
+      markers: [...this.onlineMarkers, ...this.offlineMarkers],
+      renderer: {
+        render: function (cluster) {
+          // Selecting the thumbnail image to put
+          let thumbnailUrl = "";
+          for (let i = 0; i < cluster.markers.length; i++) {
+            if (cluster.markers[i].camera.status == "online") {
+              thumbnailUrl = cluster.markers[i].camera.thumbnail_url;
+              break;
+            }
+          }
+          if (!thumbnailUrl)
+            thumbnailUrl = cluster.markers[0].camera.thumbnail_url;
+
+          // Setting the class
+          let labelStyling = 'roundUnderTen'
+          if(cluster.count >= 10)
+            labelStyling = "roundOverTen"
+          
+          // Creating the marker using the thumbnail
+          const singlecluster = new google.maps.Marker({
+            position: cluster.position,
+            icon: {
+              url: thumbnailUrl,
+              size: { width: 57, height: 31.5 },
+              scaledSize: { width: 57, height: 31.5 },
+              labelOrigin: new google.maps.Point(55,0),
+            },
+            label: {
+              text: String(cluster.count),
+              color: "rgba(255,255,255,0.9)",
+              fontSize: "12px",
+              className: labelStyling,
+              fontWeight: "700",
+            },
+            // Adjusting the zIndex to be above other markers the more markers it contains
+            zIndex: Number(google.maps.Marker.MAX_ZINDEX) + cluster.count,
+          });
+          // Creating the content for the info window
+          let content = "<div class='black--text'><ul>";
+          for (let i = 0; i < cluster.markers.length; i++) {
+            content +=
+              "<li><h3>" + cluster.markers[i].camera.name + "</h3></li>";
+          }
+          content += "</ul></div>";
+
+          // Creating the infowindow
+          const infowindow = new google.maps.InfoWindow({
+            content: content,
+          });
+
+          // Adding the infowindow to the cluster on hover
+          singlecluster.addListener("mouseover", () => {
+            infowindow.open({
+              anchor: singlecluster,
+              map: this.map,
+              shouldFocus: false,
+            });
+            // Larger marker on hover
+            singlecluster.setIcon({
+              url: singlecluster.getIcon().url,
+              size: { width: 76, height: 42 },
+              scaledSize: { width: 76, height: 42 },
+              labelOrigin: new google.maps.Point(74,0),
+            });
+            singlecluster.setZIndex(
+              singlecluster.getZIndex() +
+                (google.maps.Marker.MAX_ZINDEX)
+            );
+          });
+
+          // Closing the window when the mouse is out
+          singlecluster.addListener("mouseout", () => {
+            infowindow.close();
+            // Back to normal when out
+            singlecluster.setIcon({
+              url: singlecluster.getIcon().url,
+              size: { width: 57, height: 31.5 },
+              scaledSize: { width: 57, height: 31.5 },
+              labelOrigin: new google.maps.Point(55,0),
+            });
+            singlecluster.setZIndex(
+              singlecluster.getZIndex() -
+                (google.maps.Marker.MAX_ZINDEX)
+            );
+          });
+
+          // Pushing the cluster to the map
+          return singlecluster;
+        },
+      },
+    });
   },
 };
 </script>
@@ -152,5 +242,15 @@ export default {
 <style>
 .map {
   height: 100vh;
+}
+.roundUnderTen{
+  background-color: #1976d2;
+  padding: 4px 8px;
+  border-radius: 50%;
+}
+.roundOverTen{
+  background-color: #1976d2;
+  padding: 5px;
+  border-radius: 50%;
 }
 </style>
